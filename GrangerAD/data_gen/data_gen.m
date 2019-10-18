98c6e158e2bfe683cc00389598e17f9d93fd99d8
Niklas Preschern <niklas.preschern@gmx.at>
2019-10-18 11:08:53 +0200

    changed data gen to generate all data in one call, since otherwise the distribution of the first n generated data are different than the n:n2

--- GrangerAD/data_gen/data_gen.m
+++ GrangerAD/data_gen/data_gen.m
@@ -1,33 +1,38 @@
-function [series] = data_gen(features,lag,dependencies,n)
+function [series] = data_gen(features,lag,dependencies1,dependencies2,n1, n2)
 
 % features = 3
 % lag = 2
 % dependencies = 2
 
 % allocate space for time series
-series = zeros(features, n);
+series = zeros(features, n1+n2);
 
-% find nonzero values in dependencies matrix
-[rowIdx, colIdx] = find(dependencies~=0);
+% find nonzero values in dependencies matrix C1
+[rowIdx, colIdx] = find(dependencies1~=0);
+% find nonzero values in dependencies matrix C2
+[rowIdx2, colIdx2] = find(dependencies2~=0);
 % find which variables/features have dependencies
 deps = unique(rowIdx);
 deps_len = length(deps);
+deps2 = unique(rowIdx2);
+deps_len2 = length(deps2);
 % find how many dependencies each row has
-deps_per = sum(dependencies,2);
+deps_per = sum(dependencies1,2);
+deps_per2 = sum(dependencies2,2);
 
 % set min/max values for coefficients -> strength of
 % dependencies
 coeff_min = 0.1;
 coeff_max = 0.9;
 
-coefficients = cell(features);
+%coefficients = cell(features);
 
 %rnd_coeff = round(coeff_min+rand*(coeff_max-coeff_min),1);
 
 % generate time series that don't have any dependencies
 for i = 1:features
     if ~ismember(i,deps)
-        series(i,:) = normrnd(rand, rand, [1,n]);
+        series(i,:) = normrnd(rand, rand, [1,n1+n2]);
     end
     
 end
@@ -35,20 +40,38 @@ end
 % generate rest of the time series according to
 % dependency matrix
 for i = 1:deps_len
+    mu = rand;
+    sigma = rand;
     % gernerate random coefficient matrix depending on
     % how many dependencies a feature has (eg 3 deps -> 3x3 coeff mat)
     % rounding to one decimal in range coeff_min to coeff_max
     coeffs = round(coeff_min+rand(deps_per(i)*lag,1)*(coeff_max-coeff_min),1)
-    % generate every value from lag to n
-    for j = lag+1:n
-        % generates lagged values by multiplying dependency matrix
-        % with the time series at t-lag:t to get correct values
-        lagged_vals = nonzeros(dependencies(deps(i),:).*series(:,j-lag:j-1)');
-        series(i,j) = sum(coeffs.*lagged_vals);
+    % generate every value from 1 to n
+    for j = 1:n1
+        if j < lag+1
+           series(i,j) = normrnd(mu, sigma);
+        else
+            % generates lagged values by multiplying dependency matrix
+            % with the time series at t-lag:t to get correct values
+            lagged_vals = nonzeros(dependencies1(deps(i),:).*series(:,j-lag:j-1)');
+            series(i,j) = sum(coeffs.*lagged_vals);
+        end
     end
-    
 end
 
+for i = 1:deps_len2
+    % gernerate random coefficient matrix depending on
+    % how many dependencies a feature has (eg 3 deps -> 3x3 coeff mat)
+    % rounding to one decimal in range coeff_min to coeff_max
+    coeffs = round(coeff_min+rand(deps_per2(i)*lag,1)*(coeff_max-coeff_min),1)
+    % generate every value from 1 to n
+    for j = n1:n1+n2
+        % generates lagged values by multiplying dependency matrix
+        % with the time series at t-lag:t to get correct values
+        lagged_vals = nonzeros(dependencies1(deps2(i),:).*series(:,j-lag:j-1)');
+        series(i,j) = sum(coeffs.*lagged_vals);
+    end
+end
 
 
 end
--- GrangerAD/data_gen/gen_data.m
+++ GrangerAD/data_gen/gen_data.m
@@ -1,7 +1,7 @@
-dependencies = [0 1 1;0 0 0;0 0 0];
-dependencies2 = [0 1 0; 0 0 0; 0 0 0];
-series = data_gen(3,2,dependencies,200);
-series2 = data_gen(3,2,dependencies2,100);
+%dependencies = [0 1 1;0 0 0;0 0 0];
+%dependencies2 = [0 1 0; 0 0 0; 0 0 0];
+%series = data_gen(3,2,dependencies,200);
+%series2 = data_gen(3,2,dependencies2,100);
 
 dep1 = zeros(10);
 dep2 = zeros(10);
@@ -30,9 +30,9 @@ dep2(3,6) = 1;
 dep2(3,7) = 1;
 dep2(3,8) = 1;
 
-s1 = data_gen(10,3,dep1,200);
-s2 = data_gen(10,3,dep2,100);
-s = [s1 s2];
+s1 = data_gen(10,3,dep1,dep2,200,100);
+%s2 = data_gen(10,3,dep2,100);
+%s = [s1 s2];
 G1 = digraph(dep1);
 G2 = digraph(dep2);
-save('Datatest1_10x300.txt', s);
+save('Datatest1_10x300.txt', 's1', '-double', '-ascii');
